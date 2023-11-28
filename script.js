d66d6447281925fb5a166294cbd7ab902e623db6
eggseggstorst <126527922+eggseggstorst@users.noreply.github.com>
2023-11-28 12:46:50 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openGame() {
     var win = window.open()   
-    var url = "https://scary-fawn-tank-top.cyclic.cloud/" 
+    var url = "https://archive.org/" 
     var iframe = win.document.createElement('iframe')
     iframe.style.width = "100%";
     iframe.style.height = "100%"
